6ec87a2ca0bb2022311367da1a25bd3ace2db750
Rob Ranf <robranf@gmail.com>
2020-06-02 21:24:41 -0700

    Added timer bar, reset and done sound. Tested successfully.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     // soft egg ~ 5 min
@@ -14,11 +15,21 @@ class ViewController: UIViewController {
     // hard egg ~ 11 min
     // with water brought to a boil then lowered to a rapid simmer
     
+    var audioPlayer: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
     
     let eggTimes: [String: Int] = ["Soft": 300, "Medium": 420, "Hard": 660]
-    
     var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
@@ -28,19 +39,45 @@ class ViewController: UIViewController {
         // we then force unwrap our whole print value because it is implicitly coerced
         // from an "Int" (the value of our keys in our dict above which is what we
         // actually want to print) to a type of "Any"
+        totalTime = eggTimes[hardness!]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
-        var timeLeft = eggTimes[hardness!]
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {
-            timer in
-            
-            timeLeft! -= 1
-            print(timeLeft!)
-            
-            if(timeLeft == 0) {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+        }
+    func playSound(soundName: String) {
+        guard let sound = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            else {return}
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            audioPlayer = try AVAudioPlayer(contentsOf: sound, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let audioPlayer = audioPlayer else {return}
+            audioPlayer.play()
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = (Float(secondsPassed) / Float(totalTime))
+            print(progressBar.progress)
+        } else {
             timer.invalidate()
-            self.titleLabel.text = "DONE!"
-            }
+            playSound(soundName: "sound")
+            titleLabel.text = "DONE!"
+            
         }
-        }
+    }
+    
+    @IBAction func resetSelected(_ sender: UIButton) {
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = "How do you like your eggs?"
+    }
+    
 }
